10ac552e7d8fecde6b63fb1eb8c6962f68df1977
Hasan Bakhtiar <hasan.doctype@gmail.com>
2025-11-21 20:59:08 +0400

    lastest

---
+++ JavaScript/lesson1-introduction/app.js
@@ -0,0 +1,38 @@
+//one line
+/*
+line one
+line two
+*/
+
+// Data Types
+// Primitive Data Types
+var infoOne = "Hello World"; //string
+console.log(infoOne);
+
+var infoTwo = 5; //number
+console.log(infoTwo);
+
+var infoThree = true; //boolean (false)
+console.log(infoThree);
+
+
+
+// Composite Data Types
+var infoFour = ["Hello World",5,true]; //Array
+console.log(infoFour);
+
+var infoFive = {
+    title:"Asus Zenbook",
+    price:3000,
+    newDevice:true
+} //object
+console.log(infoFive);
+
+// Special Data Types
+var infoSix = undefined; //undefined
+console.log(infoSix);
+
+var infoSeven = null; //null
+console.log(infoSeven);
+
+
