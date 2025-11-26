a85b7fc3fdc9d158038e8f258cfabaa52a2a4ad5
Hasan Bakhtiar <hasan.doctype@gmail.com>
2025-11-26 20:53:09 +0400

    lastest

---
+++ JavaScript/lesson2-operators-variables/app.js
@@ -0,0 +1,110 @@
+
+// assignment operators
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+// **=
+
+
+
+// arithmetic operators 
+// +
+// -
+// *
+// /
+// %
+// **
+// ++
+// --
+
+
+
+
+// comparison operators
+// ==
+// ===
+// !=
+// !==
+// < 
+// <= 
+// > 
+// >= 
+
+
+
+// logical operators
+// && - AND
+// || - OR
+// !  - NOT
+
+
+
+// string operators
+// +
+// +=
+
+
+
+
+
+
+// --------------------------------------------------------------------------------------------------
+// Other (Special) Operators (Xüsusi operatorlar) Start
+
+// ,            (vergül)(kodları bir-birindən ayırmaq və sintaksis qanunauyğunluqları icra etmək üçündür )
+
+
+// \            (tərs slash)(Qaçındırma operatorudur)
+
+
+// typeof       (tip üçün)(datanın tipini təyin etmək üçündür)
+
+
+
+// void         (dəyişkənin dəyərini undefined etmək üçündür)
+
+
+
+// Other (Special) Operators (Xüsusi operatorlar) End
+// --------------------------------------------------------------------------------------------------
+
+// ECMAScript
+
+
+
+
+// 1.Dəyişkən adları mənalı və anlaşıqlı olmalıdır.
+// 2.Dəyişkən adlarını yazarkən İngilis əlifbasından başqa əlifadan istifadə etmək qadağandır.
+// 3.Dəyişkən adları _ (alt xətt) ilə başlaya bilər.
+// 4.Dəyişkən adları $(dollar) işarəsi ilə başlaya bilər.
+// 5.Dəyişkən adları heç bir zaman rəqəm ilə başlaya bilməz.Amma ilk hərfdə sonra istifadə edə bilərik.
+// 6.Dəyişkən adları böyük kiçik hərfləri anlayır.
+// 7.Eyni dəyişkənə iki fərqli məlumat tanımlanan zaman sonuncu tanımlanan məlumatı göstərir.
+// 8.JavaScript tərəfindən hazırlanmış rezerv kəlimələri dəyişkən adlarına vermək qadağandır.
+
+// abstract 	    arguments	    await*	            boolean
+// break	        byte	        case	            catch
+// char	            class*	        const	            continue
+// debugger	        default	        delete	            do
+// double	        else	        enum*	            eval
+// export*	        extends*	    false	            final
+// finally	        float	        for	                function
+// goto	            if	            implements	        import*
+// in	            instanceof	    int	                interface
+// let*	            long	        native	            new
+// null	            package	        private	            protected
+// public	        return	        short	            static
+// super*	        switch	        synchronized	    this
+// throw	        throws	        transient	        true
+// try	            typeof	        var	                void
+// volatile	        while	        with	            yield
+
+// var      -> variable (global)
+// const    -> constant (local)
+// let      -> let      (local)
+
+
+
---
+++ JavaScript/lesson3-condition/app.js
@@ -0,0 +1,112 @@
+// if - eger
+//else - deyilse
+//else if - deyilse eger
+
+// const time = 7;
+
+// if (time >= 5 && time <= 12) {
+//     console.log('good morning');
+//     if (time === 7) {
+//         console.log('Get up');
+
+//     } else if (time === 8) {
+//         console.log('Have a breakfast');
+
+//     } else if (time === 9) {
+//         console.log('Go to work');
+
+//     }
+
+// } else if (time >= 13 && time <= 17) {
+//     console.log('good afternoon');
+
+// } else if (time >= 18 && time <= 21) {
+//     console.log('good evening');
+
+// } else if (time >= 22 && time <= 23) {
+//     console.log('good night');
+
+// } else {
+//     console.log('wrong time');
+
+// }
+
+
+// const time = "Hello";
+
+// switch (time) {
+//     case 5:
+//         console.log('Time is 5');
+//         break;
+//     case "Hello":
+//         console.log('This is hello');
+//         break;
+//     default:
+//         console.log('This is default');
+//         break;
+// }
+
+
+
+
+
+
+
+
+
+
+// let info = 5;
+
+// switch (info) {
+//     case 5:
+//         console.log('This is 5');
+//         info = 1;
+//         if (info < 3) {
+//             console.log('This case is true');
+
+//         } else {
+
+//         }
+//         break;
+
+//     default:
+//         console.log('This case is default');
+
+//         break;
+// }
+// info = 1;
+// if (info < 2) {
+//     info = 5
+//     switch (info) {
+//         case 5:
+//             console.log('This is 5');
+
+//             break;
+
+//         default:
+//             break;
+//     }
+// } else {
+
+// }
+
+
+// if(5<10)
+//     console.log('this is true');
+// else
+//     console.log('This is false');
+    
+    
+
+
+// try {
+//     throw('Her sey yaxshidir')
+//     consoale.log('Hello Wolrd');
+    
+// } catch (error) {
+//     console.log(error);
+    
+// }finally{
+//     console.log('men hemishe ishleyirem ha ha ha');
+    
+// }
