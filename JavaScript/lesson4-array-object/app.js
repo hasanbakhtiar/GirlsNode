e5316671d33d926a5b1316e3c07ac310e022540e
Hasan Bakhtiar <hasan.doctype@gmail.com>
2025-11-29 14:29:03 +0400

    loops

---
+++ JavaScript/lesson4-array-object/app.js
@@ -0,0 +1,66 @@
+// const brands = ["Asus", "Msi", "Lenovo", "Hp", "Apple", "Dell", "Acer"];
+
+// brands[0] = "New data";
+// brands['mykey']='new value';
+// brands['mykey2']='new value';
+// brands[7] = 'my data';
+// console.log(brands.length);
+
+// const brands = [];
+// brands[0]="Asus";
+// brands[1]="Msi";
+// console.log(brands);
+
+
+
+// const info = [1,2,3,['data',['summer',[true,false],'winter'],'value'],4,5];
+// console.log(info[3][1][1][0]);
+
+// const brands = ["Asus", "Msi", "Lenovo", "Acer"];
+// brands.pop()
+// brands.push('new value')
+// brands.shift()
+// brands.unshift('new value')
+// brands.length;
+// console.log(brands);
+
+
+
+// const car = {
+//     title: "BMW X5",
+//     pirce: "37000",
+//     newCar: false,
+//     stock: 1,
+//     color:['white','black','pink']
+// }
+
+// console.log(car.color[0]);
+
+
+
+
+const laptops = [
+    {
+        title:"Asus Zenbook",
+        price:3000,
+        newDevice:true
+    },
+    {
+        title:"Acer Swift",
+        price:2700,
+        newDevice:false
+    },
+    {
+        title:"Dell XPS",
+        price:5400,
+        newDevice:true
+    }
+]
+console.log(laptops[0].title);
+console.log(laptops[1].title);
+console.log(laptops[2].title);
+
+
+
+
+
---
+++ JavaScript/lesson5-loops/app.js
@@ -0,0 +1,103 @@
+
+let count = 0;
+
+// while (count > 5) {//condition
+//     console.log(count);//result
+//     count++;//action
+// }
+
+
+// do {
+//     count++;//action
+//     console.log(count);//result
+
+// } while (count > 5)//condition
+
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+
+
+
+
+const laptops = [
+    {
+        title:"Asus Zenbook",
+        price:3000,
+        category:"Premium",
+        color:['black',"blue",'pink'],
+        newDevice:true
+    },
+    {
+        title:"Acer Swift",
+        price:2700,
+        category:"Office",
+        color:['black',"red",'ornage'],
+        newDevice:false
+    },
+    {
+        title:"Dell XPS",
+        price:5400,
+        category:"Premium",
+        color:['green',"blue",'ornage'],
+        newDevice:true
+    },
+    {
+        title:"MSI Sword",
+        price:4400,
+        category:"Gaming",
+        color:['black',"gray",'ornage'],
+        newDevice:false
+    },
+    {
+        title:"Asus Rog Strix",
+        price:7400,
+        category:"Gaming",
+        color:['black',"blue",'white'],
+        newDevice:true
+    }
+]
+
+
+// for(let count in laptops){
+//     console.log(laptops[count].title);
+    
+// }
+
+// for (let item of laptops) {
+//         console.log(item);
+        
+// }
+
+
+
+
+// for (let i = 0; i < laptops.length; i++) {
+    
+//     for (let c = 0; c < laptops[i].color.length; c++) {
+//         if (laptops[i].color[c] == 'blue') {
+//             console.log(laptops[i].title);
+            
+//         }
+        
+        
+//     }
+   
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
